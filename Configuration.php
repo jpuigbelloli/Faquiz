f9f635a7fa5e056bd560d49f4aec2c8374e7943b
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-05-29 17:37:50 -0300

    borrados

--- Configuration.php
+++ Configuration.php
@@ -3,14 +3,10 @@ include_once('helpers/MySqlDatabase.php');
 include_once("helpers/MustacheRender.php");
 include_once('helpers/Router.php');
 
-include_once ("model/ToursModel.php");
 include_once('model/SongsModel.php');
 include_once('model/UsuarioModel.php');
 
-include_once('controller/ToursController.php');
-include_once('controller/SongsController.php');
-include_once('controller/LaBandaController.php');
-include_once('controller/UserController.php');
+
 include_once('controller/LoginController.php');
 
 include_once('third-party/mustache/src/Mustache/Autoloader.php');
