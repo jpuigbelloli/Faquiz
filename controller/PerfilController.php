e1aa0017ff4553b63ad62ab7e3fe9b29da040982
GON-CAB-8 <cabralmartin8@gmail.com>
2023-06-08 19:58:13 -0300

    se puede acceder a perfil de usuario si esta logueado y se puede acceder a cualquier perfil publicamente con /perfil/**usuario**

--- controller/PerfilController.php
+++ controller/PerfilController.php
@@ -1,25 +1,31 @@
 <?php
 
-class PerfilController {
+class PerfilController
+{
     private $perfilModel;
     private $renderer;
 
-    public function __construct($perfilModel, $renderer) {
+
+    public function __construct($perfilModel, $renderer)
+    {
         $this->perfilModel = $perfilModel;
         $this->renderer = $renderer;
     }
 
-    public function list() {
-        $data["usuario"] = $this->perfilModel->getData($_SESSION['usuario']);
-        $this->renderer->render("perfil", $data);
+    public function list()
+    {
+        var_dump($_SESSION);
+        $nombreDeUsuario = $_GET['usuario'] ?? $_SESSION['usuario'];
+        $usuario = $this->perfilModel->getData($nombreDeUsuario);
+
+        if ($usuario) {
+            $data = array("usuario" => $usuario);  // Utiliza la clave "usuario" en lugar de la variable $usuario
+            $this->renderer->render("perfil", $data);
+        } else {
+            //HABRIA QUE HACER UNA VISTA ERROR PARA TIRAR TODOS LOS ERRORES A ESA VISTA
+            echo "Perfil no encontrado";
+        }
     }
 
-    public function add(){
-        die("llame a add");
-    }
-
-    public function delete() {
-        die('llame a delete');
-    }
 }
 
