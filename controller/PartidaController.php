58d69f23dff32f468c2b7d88b6b830aeeeda987d
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-06-10 01:59:20 -0300

    partida mostrando correcta o incorrectas

--- controller/PartidaController.php
+++ controller/PartidaController.php
@@ -23,33 +23,44 @@ class PartidaController{
     {
 
         if (!isset($_SESSION['logueado']) || $_SESSION['logueado'] !== true) {
-            header('Location: /login');
+            header('Location:/login');
             exit();
         }
 
-        $data["pregunta_respuestas"] = $this->partidaModel->obtenerPreguntasYRespuestas();
-        $this->respuestaCorrecta = $data["pregunta_respuestas"][0]['correcta'];
+        if(isset($_GET["correcto"]) || isset($_GET["incorrecto"])){
+            $data["correcto"]=$_GET["correcto"]??"";
+            $data["incorrecto"]=$_GET["incorrecto"]??"";
+        }
+//        $data["pregunta_respuestas"] = $this->partidaModel->obtenerPreguntasYRespuestas();
+        $data["pregunta"] = $this->partidaModel->obtenerPregunta();
+        $idPregunta = $data["pregunta"][0]["id_pregunta"];
+        $data["respuestas"] = $this->partidaModel->obtenerRespuestas($idPregunta);
+//        echo '<pre>';
+//        var_dump($data);
+//        echo '</pre>';
+//        $this->respuestaCorrecta = $data["pregunta_respuestas"][0]['correcta'];
 //        '<script src="./public/js/reload.js"></script>';
-        $this->id = $data["pregunta_respuestas"][0]["id_pregunta"];
-        $this->responder();
-        var_dump($this->id);
-        $this->renderer->render('partida', $data);
+//        $this->responder();
+        $this->renderer->render('partida',$data);
     }
 
     public function responder()
     {
         $respuesta = $_GET['opcion'];
-        $id = $this->getId();
-        if (isset($respuesta)) {
-            if ($this->esCorrecta($id,$respuesta)){
-                $this->puntos++;
-                $this->puntaje += $this->puntos;
-                echo "Correcto!";
+        $id_pregunta = $_GET['id'];
 
-            } else {
-                echo "Respuesta Incorrecta";
-                $this->puntos = 0;
-                var_dump($respuesta);
+        $correcta = $this->partidaModel->esCorrecta($id_pregunta,$respuesta);
+
+        if ($correcta[0]["correcta"] == 1){
+            $this->puntos++;
+            $this->puntaje += $this->puntos;
+            header("Location:/partida?correcto=1");
+            exit();
+        } else {
+            header("Location:/partida?incorrecto=1");
+            exit();
+            $this->puntos = 0;
+
 
 //                $usuario = $_SESSION['usuario'];
 //                $puntaje= $_GET['puntos'];
@@ -57,8 +68,8 @@ class PartidaController{
 
 
 
-            }
         }
+
     }
     public function getId(): int
     {
--- model/PartidaModel.php
+++ model/PartidaModel.php
@@ -16,6 +16,32 @@ class PartidaModel{
     return $this->database->query_assoc($sql);
     }
 
+    public function obtenerPregunta(){
+        $sql = "SELECT P.id_pregunta AS id_pregunta, P.pregunta AS pregunta, C.descripcion AS categoria FROM pregunta P
+                JOIN categoria C
+                ON P.id_categoria = C.id_categoria
+                ORDER BY RAND() LIMIT 1";
+
+        return $this->database->query_assoc($sql);
+    }
+
+    public function obtenerRespuestas($id){
+        $sql = "SELECT R.respuesta AS respuesta
+                FROM respuesta R
+                WHERE R.id_pregunta = '$id'";
+
+        return $this->database->query_assoc($sql);
+    }
+
+    public function esCorrecta($id,$respuesta){
+        $sql = "SELECT R.correcta FROM respuesta R
+                WHERE R.respuesta = '$respuesta'
+                AND R.id_pregunta = '$id'";
+        //se podria sacar el AND
+
+        return $this->database->query_assoc($sql);
+    }
+
     public function agregarPartida($usuario,$puntero){
          $sql = "INSERT INTO partida (id_usuario, puntaje) VALUES 
                                        ($usuario,$puntero);";
