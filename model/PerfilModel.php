58b048fbae55b276469bb3187d17335786f193f7
GON-CAB-8 <cabralmartin8@gmail.com>
2023-06-28 20:41:42 -0300

    cambios y mejoras a perfil controller y perfil model. cambiado el comportamiento del div encargado de mostrar el avatar

--- model/PerfilModel.php
+++ model/PerfilModel.php
@@ -10,95 +10,67 @@ class PerfilModel
         $this->database = $database;
     }
 
-   /* private function obtenerIDUsuario()
+    /* private function obtenerIDUsuario()
+     {
+         return 32;
+     }*/
+
+    public function getData($nombreDeUsuario)
     {
-        return 32;
-    }*/
+        $resultado = $this->database->query("SELECT user_name, foto_perfil AS fotoPerfil, nombre, apellido, email, fecha_nac FROM usuario WHERE user_name = '$nombreDeUsuario'");
 
-    //DEPRECADO **BORRAR**
-/*    public function getData($dato)
-    {
-        $resultado= $this->database->query("SELECT user_name, foto_perfil AS fotoPerfil,CONCAT(nombre,SPACE(1),apellido) AS nombre,
-                                            email ,YEAR(current_date()) - YEAR(fecha_nac) AS edad
-                                            FROM usuario
-                                            WHERE user_name ='$dato'");
-
-        if($resultado && $resultado->num_rows > 0){
-            $usuario = $resultado->fetch_assoc();
-        }
-         return $usuario;
-
-    }*/
-
-    public function getData($nombreDeUsuario) {
-        $resultado = $this->database->query("SELECT user_name, foto_perfil AS fotoPerfil,CONCAT(nombre,SPACE(1),apellido) AS nombre,
-                                        email ,YEAR(current_date()) - YEAR(fecha_nac) AS edad
-                                        FROM usuario 
-                                        WHERE user_name ='$nombreDeUsuario'");
-
-        if($resultado && $resultado->num_rows > 0) {
+        if ($resultado && $resultado->num_rows > 0) {
             $usuario = $resultado->fetch_assoc();
             return $usuario;
         }
-
         return null;
     }
 
-    public function getDireccionQR($usuario){
+
+    public function getArray($nombreDeUsuario)
+    {
+        $usuario = $this->getData($nombreDeUsuario);
+        if (!$usuario) {
+            return null; // Usuario no encontrado
+        }
+
+        $arrayDatos = [
+            'user_name' => $usuario['user_name'],
+            'fotoPerfil' => $usuario['fotoPerfil'],
+            'nombreCompleto' => $this->generarNombreCompleto($usuario['nombre'], $usuario['apellido']),
+            'email' => $usuario['email'],
+            'edad' => $this->calcularEdad($usuario['fecha_nac']),
+            'rutaQR' => $this->getDireccionQR($usuario),
+            'ubicacion' => $this->getCoordenadasUsuario($usuario),
+        ];
+
+        return $arrayDatos;
+    }
+
+    private function generarNombreCompleto($nombre, $apellido)
+    {
+        return $nombre . ' ' . $apellido;
+    }
+
+    private function calcularEdad($fechaNacimiento)
+    {
+        $fechaActual = new DateTime();
+        $fechaNacimiento = new DateTime($fechaNacimiento);
+        $edad = $fechaNacimiento->diff($fechaActual)->y;
+
+        return $edad;
+    }
+
+
+
+    public function getDireccionQR($usuario)
+    {
 
         $directorioQR = 'public/codigosQR/';
-        $nombreArchivoQR = $usuario . ".png";
+        $nombreArchivoQR = $usuario['user_name'] . ".png";
 
 
         return $directorioQR . $nombreArchivoQR;
-    }
-
-    public function generarArray()
-    {
-        /*$arrayDatos = [];
-        $user_name = $this->getData('user_name');
-        array_push($arrayDatos, $user_name);
-        $foto_perfil = $this->getData('foto_perfil');
-        array_push($arrayDatos, $foto_perfil);
-        $nombreCompleto = $this->generarNombreCompuesto();
-        array_push($arrayDatos, $nombreCompleto);
-        $email = $this->getData('email');
-        array_push($arrayDatos, $email);
-        $edad = $this->calcularEdad();
-        array_push($arrayDatos, $edad);
-        $pais = $this->obtenerPais();
-        array_push($arrayDatos, $pais);
-
-        return $arrayDatos;*/
-    }
-
-    private function generarNombreCompuesto()
-    {
-       /* $userID = $this->obtenerIDUsuario();
-
-        $nombre = $this->getData('nombre');
-        $apellido = $this->getData('apellido');
-
-        return $nombre . ' ' . $apellido;*/
-    }
-
-    private function calcularEdad()
-    {
-//        $userID = $this->obtenerIDUsuario();
-//
-//        $fechaNacimiento = $this->getData($userID, 'fecha_nac');
-//
-//        $fechaActual = new DateTime();
-//        $fechaNacimiento = new DateTime($fechaNacimiento);
-//        $edad = $fechaNacimiento->diff($fechaActual)->y;
-//
-//        return $edad;
-
-    }
-
-    private function obtenerPais()
-    {
-        return "Argentina";
     }
 
     public function existeUsuario($nombreDeUsuario)
@@ -110,18 +82,20 @@ class PerfilModel
         return $totalUsuarios > 0;
     }
 
-    public function getCoordenadasUsuario($usuario){
+    public function getCoordenadasUsuario($usuario)
+    {
         $resultado = $this->database->query("SELECT ubicacion                             
                                              FROM usuario 
-                                             WHERE user_name ='$usuario'");
+                                             WHERE user_name ='".$usuario['user_name']."'");
 
-        if($resultado && $resultado->num_rows > 0) {
+        if ($resultado && $resultado->num_rows > 0) {
             $ubicacion = $resultado->fetch_assoc();
-            return $ubicacion;
+            return $ubicacion['ubicacion'];
         }
 
         return null;
     }
+
 
 
 }
