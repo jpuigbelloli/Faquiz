5be361f71994b6cfb7c52cd0c470f8a306070915
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-07-02 17:59:16 -0300

    entregando preguntas por nivel de dificultad

--- model/PartidaModel.php
+++ model/PartidaModel.php
@@ -8,22 +8,89 @@ class PartidaModel{
         $this->database = $database;
     }
 
-    public function obtenerPreguntasYRespuestas(){
-    $sql = "SELECT id_pregunta,pregunta, respuesta_A AS A,respuesta_B AS B, respuesta_C AS C,respuesta_d AS D, C.descripcion categoria, P.respuesta_Correcta correcta
-            FROM pregunta P INNER JOIN 
-                categoria C ON P.id_categoria = C.id_categoria
-            ORDER BY RAND() LIMIT 1";
-    return $this->database->query_assoc($sql);
+    public function getNivelJugador($id){
+        $sql = "SELECT id_usuario,respuesta
+                FROM estadistica
+                WHERE id_usuario = '$id'";
+        $resultado = $this->database->query_assoc($sql);
+
+        $correctas = 0;
+        $total = 0;
+
+        foreach ($resultado as $result){
+            if ($result['respuesta'] === '1'){
+                $correctas++;
+            }
+            $total++;
+        }
+        //esto quiere decir que el jugador no tiene nivel porque nunca jugó
+        if($total === 0){
+            return null;
+        }
+        return $correctas / $total * 100;
     }
+
+    public function getIDPreguntaSegunNivelJugador($id){
+        $nivelJugador = $this->getNivelJugador($id);
+
+        Logger::info('Mi nivel es '.$nivelJugador);
+        if($nivelJugador >=0 && $nivelJugador<=33 || $nivelJugador === null){
+            $sql = "SELECT P.id_pregunta, e.porcentaje
+                    FROM pregunta P
+                    LEFT JOIN (
+                        SELECT id_pregunta,
+                               (SUM(CASE WHEN respuesta = '1' THEN 1 ELSE 0 END) / COUNT(*) * 100) AS porcentaje
+                        FROM estadistica
+                        GROUP BY id_pregunta) e 
+                        ON P.id_pregunta = e.id_pregunta
+                        WHERE P.id_pregunta NOT IN( SELECT id_pregunta FROM estadistica E
+                                            WHERE E.respuesta = '1' AND E.id_usuario = '$id')
+                        AND P.agregada = 1
+                    HAVING porcentaje >=66 AND porcentaje <=100 OR porcentaje IS NULL";
+            Logger::info('ME DIERON PREGUNTA FACIL');
+        } else if($nivelJugador >33 && $nivelJugador <=66){
+            $sql = "SELECT P.id_pregunta, e.porcentaje
+                    FROM pregunta P
+                    LEFT JOIN (
+                        SELECT id_pregunta,
+                               (SUM(CASE WHEN respuesta = '1' THEN 1 ELSE 0 END) / COUNT(*) * 100) AS porcentaje
+                        FROM estadistica
+                        GROUP BY id_pregunta) e 
+                        ON P.id_pregunta = e.id_pregunta
+                        WHERE P.id_pregunta NOT IN( SELECT id_pregunta FROM estadistica E
+                                            WHERE E.respuesta = '1' AND E.id_usuario = '$id')
+                        AND P.agregada = 1
+                    HAVING porcentaje >=33 AND porcentaje <66 OR porcentaje IS NULL";
+            Logger::info('ME DIERON PREGUNTA MEDIA');
+        } else {
+            $sql = "SELECT P.id_pregunta, e.porcentaje
+                    FROM pregunta P
+                    LEFT JOIN (
+                        SELECT id_pregunta,
+                               (SUM(CASE WHEN respuesta = '1' THEN 1 ELSE 0 END) / COUNT(*) * 100) AS porcentaje
+                        FROM estadistica
+                        GROUP BY id_pregunta) e 
+                        ON P.id_pregunta = e.id_pregunta
+                        WHERE P.id_pregunta NOT IN( SELECT id_pregunta FROM estadistica E
+                                            WHERE E.respuesta = '1' AND E.id_usuario = '$id')
+                    HAVING porcentaje >=0 AND porcentaje <33 OR porcentaje IS NULL";
+            Logger::info('ME DIERON PREGUNTA DIFICIL');
+        }
+        $resultado = $this->database->query_assoc($sql);
+        $randomKey= array_rand($resultado);
+        $idPreguntaRandom = $resultado[$randomKey];
+
+        return $idPreguntaRandom['id_pregunta'];
+    }
+
     //obtiene las preguntas que el usuario no respondio y las que fueron agregadas
     public function obtenerPregunta($id){
+
+        $id_pregunta = $this->getIDPreguntaSegunNivelJugador($id);
         $sql = "SELECT P.id_pregunta AS id_pregunta, P.pregunta AS pregunta, C.descripcion AS categoria FROM pregunta P
                 JOIN categoria C
                 ON P.id_categoria = C.id_categoria
-                WHERE P.id_pregunta NOT IN( SELECT id_pregunta FROM estadistica E
-                                            WHERE E.respuesta = '1' AND E.id_usuario = '$id')  
-                AND P.agregada = 1
-                ORDER BY RAND() LIMIT 1";
+                WHERE P.id_pregunta = '$id_pregunta'";
 
         return $this->database->query_assoc($sql);
     }
@@ -78,4 +145,20 @@ class PartidaModel{
                 VALUES ('$id_pregunta','$motivo','$id_usuario')";
         $this->database->query($sql);
     }
+
+
+    /*
+     * SELECT p.id_pregunta, e.porcentaje
+                    FROM pregunta p
+                    LEFT JOIN (
+                        SELECT id_pregunta,
+                               (SUM(CASE WHEN respuesta = '1' THEN 1 ELSE 0 END) / COUNT(*) * 100) AS porcentaje
+                        FROM estadistica
+                        GROUP BY id_pregunta) e
+                        ON p.id_pregunta = e.id_pregunta
+                        WHERE P.id_pregunta NOT IN( SELECT id_pregunta FROM estadistica E
+                                            WHERE E.respuesta = '1' AND E.id_usuario = 5)
+                    HAVING porcentaje >=66 AND porcentaje <=100 OR porcentaje IS NULL
+ORDER BY `p`.`id_pregunta` ASC*/
+
 }
