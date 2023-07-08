d78e725d0152303cf84b048be4df55af4d7d1e68
Florencia Galvan <flgalvan@alumno.unlam.edu.ar>
2023-07-08 03:56:39 -0300

    Se muestran graficos 1 y 2

--- controller/ReporteController.php
+++ controller/ReporteController.php
@@ -1,6 +1,7 @@
 <?php
-require_once "./third-party/jpgraph/src/jpgraph.php";
-require_once "./third-party/jpgraph/src/jpgraph_bar.php";
+ require_once "./third-party/jpgraph/src/jpgraph.php";
+ require_once "./third-party/jpgraph/src/jpgraph_bar.php";
+require_once './third-party/jpgraph/src/jpgraph_pie.php';
 
 class ReporteController{
     private $reporteModel;
@@ -12,58 +13,98 @@ class ReporteController{
     }
 
     public function list(){
+
         if(!isset($_SESSION['logueado']) || Usuario::getROL()!=='ADMINISTRADOR'){
             header('Location:/lobby');
             exit();
         }
+
         if(isset($_POST['filtro']) && isset($_POST['buscar'])){
             $filtro = $_POST['filtro'];
         }
+        else {
+            $filtro = "Month";
+        }
+
         $data['jugador'] = $this->reporteModel->cantidadDeUsuarios($filtro);
         $data['nuevos'] = $this->reporteModel->cantidadDeUsuariosNuevos($filtro);
         $data['genero'] = $this->reporteModel->cantidadJugadoresPorGenero();
         $data['edad'] = $this->reporteModel->cantidadJugadoresPorEdad();
+        $data['pais'] = $this->reporteModel->jugadoresPorPais();
         $data['partidas'] = $this->reporteModel->getCantidadDePartidas($filtro);
-        $data['preg_totales'] = $this->reporteModel->getCantPreguntasAdmin();
-        $data['preg_nuevas'] = $this->reporteModel->getCantNuevas();
+        $data['preg_totales'] = $this->reporteModel->getCantPreguntasAdmin($filtro);
+        $data['preg_nuevas'] = $this->reporteModel->getCantNuevas($filtro);
+        $data['preg_usr']   = $this->reporteModel->getRespuestaPorUsuario();
 
-        $this->renderer->render('admin',$data);
+       $this->getGraficoBarra($data['jugador'],'cantidadJugadores.png','Cantidad de Jugadores');
+       $this->getGraficoBarra($data['nuevos'],'cantidadJugadoresNuevos.png','Cantidad de Jugadores Nuevos');
+       $this->getGraficoPie($data['genero'], 'cantidadPorGenero.png', 'Jugadores por genero');
+       $this->getGraficoPie($data['edad'],'cantidadPorGrupoEtario.png','Cantidad por Grupo Etario');
+       $this->getGraficoPie($data['pais'],'cantidadPorPais.png','Cantidad de Jugadores por Pais');
+       $this->getGraficoBarra($data['partidas'],'cantidadPartidas.png','Cantidad de Partidas');
+       $this->getGraficoBarra($data['preg_totales'],'preguntasTotales.png','Preguntas Totales');
+       $this->getGraficoBarra($data['preg_nuevas'],'preguntasNuevas.png','Cantidad de Preguntas nuevas');
+       $this->getGraficoBarra($data['preg_usr'], 'porcentajeUsuario.png','Porcentaje Correctas por Usuario');
+
+       $this->renderer->render('admin',$data);
     }
 
-    public function getGraficoJugadores(){
-
-        if(isset($_POST['filtro'])){
-            $filtro = $_POST['filtro'];
-        }
-
-        $data['jugador'] = $this->reporteModel->cantidadDeUsuarios($filtro);
+    public function getGraficoBarra($data,$filename,$titulo){
 
         $data1y = array();
         $dataName = array();
 
-
         for ($i = 0; $i < count($data); $i++) {
             $data1y[] = $data[$i]["cantidad"];
-            $dataName = $data[$i]["filtro"];
+            $dataName[] = $data[$i]["filtro"];
         }
+
         $graph = $this->getGraph();
         $this->getConfigGraph($graph,$dataName);
 
         $b1plot = $this->getBarPlot($data1y);
         $graph->Add($b1plot);
 
-        $graph->title->Set('Cantidad de Jugadores');
-
-        $graph->Stroke(_IMG_HANDLER);
-
-        $nombreImg = 'public/graficos/cantidadJugadores.png';
-
-        $graph->img->Stream($nombreImg);
-
-        $graph->img->Headers();
-        $graph->img->Stream();
+        $graph->title->Set($titulo);
+        $nombreImg = 'public/graficos/'.$filename;
+        $this->getImagenGrafico($graph,$nombreImg);
+       // $nombreImg = 'public/graficos/'.$filename;
+        $graph->Stroke();
 
 
+    }
+
+    public function getGraficoPie($data,$filename,$titulo){
+
+        $data1y = array();
+        $dataName = array();
+
+        for ($i = 0; $i < count($data); $i++) {
+            $data1y[] = $data[$i]["cantidad"];
+            $dataName[] = $data[$i]["filtro"];
+        }
+        $graph = new PieGraph(400, 300,'auto');
+        $graph->SetScale("textlin");
+        $theme_class = new UniversalTheme;
+        $graph->SetTheme($theme_class);
+        $graph->SetBox(false);
+        $graph->ygrid->SetFill(false);
+        $graph->xaxis->SetTickLabels($dataName);
+
+        $pieplot = new PiePlot($data1y);
+        $pieplot->SetStartAngle(30);
+        $pieplot->SetLegends(array_keys($dataName));
+        $pieplot->SetSliceColors(array('red', 'blue', 'green'));
+        $pieplot->value->SetFont(FF_ARIAL, FS_NORMAL, 12);
+        $pieplot->value->SetColor('black');
+        $pieplot->value->Show();
+
+        $graph->Add($pieplot);
+
+        $graph->title->Set($titulo);
+        $nombreImg = 'public/graficos/'.$filename;
+        $this->getImagenGrafico($graph,$nombreImg);
+        $graph->Stroke();
     }
 
     public function getGraph(){
@@ -89,6 +130,12 @@ class ReporteController{
         $b1plot->value->Show();
 
         return $b1plot;
+    }
+
+    public function getImagenGrafico($graph,$fileName){
+        $graph->Stroke(_IMG_HANDLER);
+        $image = $graph->img->Stream($fileName);
+        return $image;
     }
 
 }
--- model/ReporteModel.php
+++ model/ReporteModel.php
@@ -19,7 +19,7 @@ class ReporteModel{
                 END filtro
                  FROM usuario
                 WHERE id_rol = 1
-                GROUP BY DATE(fecha_ingreso);
+                GROUP BY filtro;
                  ";
         return $this->database->query_assoc($sql);
     }
@@ -35,7 +35,7 @@ class ReporteModel{
                  FROM usuario
                 WHERE id_rol = 1
                 AND fecha_ingreso >= DATE_SUB(CURDATE(), INTERVAL 3 DAY) 
-                GROUP BY DATE(fecha_ingreso);
+                GROUP BY filtro;
                  ";
         return $this->database->query_assoc($sql);
     }
@@ -45,9 +45,9 @@ class ReporteModel{
                                             WHEN genero = 1 THEN 'Femenino'
                                             WHEN genero = 2 THEN 'Masculino'
                                             ELSE 'Sin Especificar' 
-                                          END genero
+                                          END filtro
                 FROM usuario
-                GROUP BY genero;";
+                GROUP BY filtro;";
 
        return $this->database->query_assoc($sql);
     }
@@ -57,29 +57,43 @@ class ReporteModel{
                                             WHEN YEAR(current_date()) - YEAR(fecha_nac) <18 THEN 'Menor'
                                             WHEN YEAR(current_date()) - YEAR(fecha_nac)>18 && YEAR(current_date()) - YEAR(fecha_nac) <60 THEN 'Medio'
                                             WHEN YEAR(current_date()) - YEAR(fecha_nac) > 60 THEN 'Jubilado' 
-                                          END grupoEtario
+                                          END filtro
                 FROM usuario
-                GROUP BY grupoEtario;";
+                GROUP BY filtro;";
 
         return $this->database->query_assoc($sql);
     }
 
+    public function jugadoresPorPais(){
+        $sql = "SELECT COUNT(*) cantidad , pais filtro
+                FROM usuario
+                GROUP by pais;";
+        return $this->database->query_assoc($sql);
+    }
+
     public function getCantPreguntasAdmin(){
-        $sql = "SELECT COUNT(id_pregunta) cant_preguntas  
-                FROM pregunta;
+        $sql = "SELECT COUNT(*) cantidad, C.descripcion filtro 
+                FROM pregunta P INNER JOIN
+                    categoria C 
+                        ON P.id_categoria = C.id_categoria
+                GROUP BY filtro
                 ";
-        return $this->database->query($sql);
+        return $this->database->query_assoc($sql);
     }
 
     public function getCantNuevas(){
-        $sql = "SELECT COUNT(id_pregunta) cant_preguntas
-                FROM pregunta_sugerida
-                WHERE id_estado = 1;
+        $sql = "SELECT COUNT(*) cantidad, C.descripcion filtro
+                FROM pregunta_sugerida P  INNER JOIN
+                    categoria C
+                        ON P.id_categoria = C.id_categoria
+                WHERE P.id_estado = 2
+                GROUP BY filtro;
                 ";
+        return $this->database->query_assoc($sql);
     }
 
     public function getCantidadDePartidas($filtro){
-        $sql = "SELECT  DATE_FORMAT(DATE (fecha),'%d-%m-%y') fecha, COUNT(*) cant_partidas, 
+        $sql = "SELECT  DATE_FORMAT(DATE (fecha),'%d-%m-%y') fecha, COUNT(*) cantidad, 
                 CASE 
                     WHEN '$filtro' ='Month' THEN MONTH(fecha) 
                     WHEN '$filtro' ='Year' THEN YEAR(fecha) 
@@ -87,9 +101,26 @@ class ReporteModel{
                     WHEN '$filtro' ='Week' THEN WEEK(fecha) 
                 END filtro
                 FROM partida
-                GROUP BY fecha";
-        return $this->database->query($sql);
+                GROUP BY filtro";
+        return $this->database->query_assoc($sql);
 
     }
 
+    public function getJugadorPorPais(){
+        $sql = "SELECT COUNT(*) cantidad, pais filtro
+                 FROM usuario
+                ";
+        return $this->database->query_assoc($sql);
+    }
+
+    public function getRespuestaPorUsuario(){
+        $sql = "SELECT U.user_name cantidad, (SUM(CASE WHEN respuesta = '1' THEN 1 ELSE 0 END) / COUNT(*) * 100) AS filtro 
+                FROM estadistica E INNER JOIN 
+                    usuario U 
+                        ON E.id_usuario = U.id_usuario 
+                GROUP BY id_pregunta 
+                ORDER BY filtro DESC;";
+
+        return $this->database->query_assoc($sql);
+    }
 }
